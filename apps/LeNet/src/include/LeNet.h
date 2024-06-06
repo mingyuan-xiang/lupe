9305133d221b9edc5fcc1f15c1763a9c84dce37f
Mingyuan Xiang <myxiang@uchicago.edu>
2024-06-06 15:31:52 -0500

    remove LeNet

--- apps/LeNet/src/include/LeNet.h
+++
@@ -1,9 +0,0 @@
-#ifndef LENET_MODEL_H
-#define LENET_MODEL_H
-
-#include <stdint.h>
-#include <libmatAbstract/mat.h>
-
-uint16_t LeNet(mat_t* image);
-
-#endif
--- apps/LeNet/src/layers/Flatten.c
+++
@@ -1,8 +0,0 @@
-#include <layers/include/utils.h>
-#include <layers/include/Flatten.h>
-#include <buffer/include/buffer.h>
-
-void Flatten(mat_t* input, mat_t* output) {
-/* The data is stored in 1D array. So, we just need to feed the input to the output*/
-  memcpy(output->data, input->data, MAT_GET_SIZE(input)*sizeof(uint16_t));
-}
