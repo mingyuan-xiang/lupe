e8d9dc74219cc746a9acc256ba84b1a593961feb
Mingyuan Xiang <myxiang@uchicago.edu>
2024-07-24 12:44:59 -0500

    add calibration script

---
+++ calibration/calibration.py
@@ -0,0 +1,9 @@
+"""UART receiver for calibrating adaptive layer generation."""
+
+def calibrating():
+    """Get the acceleration choices for each layer
+    
+    Returns:
+        Return dictionary of layer names and acceleration choices
+    """
+    return {}
