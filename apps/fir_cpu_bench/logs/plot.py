7af2f86e1aa9c474a4f30e491ceebbda14e649b3
Mingyuan Xiang <myxiang@uchicago.edu>
2024-08-15 14:07:22 -0700

    add fir_cpu_bench results

--- apps/fir_cpu_bench/logs/plot.py
+++ apps/fir_cpu_bench/logs/plot.py
@@ -8,51 +8,50 @@ import numpy as np
 
 freq = 32767
 
-io = 'in'
-op = 'fir'
-kernel_size = 5
-
 def parse_data(data):
     pattern = r"total cycles \((?P<frequency>\d+ Hz)\), repeat for (?P<repeat>\d+) times: (?P<time>\d+) \(kernel size: (?P<kernel_size>\d+), in_channels: (?P<in_channels>\d+), out_channels: (?P<out_channels>\d+), input size: (?P<input_size>\d+)\)"
     matches = re.findall(pattern, data, flags=re.DOTALL)
     results = []
     for _, repeat, cycles, kernel_size, in_channels, out_channels, input_size in matches:
-        if io == 'in':
-            ch = in_channels
-        else:
-            ch = out_channels
+        k = int(kernel_size)
+        i_ch = int(in_channels)
+        o_ch = int(out_channels)
+        i_s = int(input_size)
+        o_s = i_s - k + 1
+
         results.append({
             'time': int(cycles) / (int(repeat) * freq),
-            f'{io}_channels': int(ch),
-            'input_size': int(input_size),
+            'flops': k * k * i_ch * o_s * o_s * o_ch / (10**6),
         })
 
     return results
 
-with open(f'{op}_conv_{kernel_size}.log') as f:
-    data = f.read()
-    data = parse_data(data)
+with open(f'fir_conv.log') as f:
+    fir_data = f.read()
+    fir_data = parse_data(fir_data)
+fir_df = pd.DataFrame(fir_data)
+
+with open(f'cpu_conv.log') as f:
+    cpu_data = f.read()
+    cpu_data = parse_data(cpu_data)
+cpu_df = pd.DataFrame(cpu_data)
 
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams.update({'font.size': 14})
 plt.rcParams["font.weight"] = "bold"
 plt.rcParams["axes.labelweight"] = "bold"
 
-df = pd.DataFrame(data)
-
 fig = plt.figure()
 fig.add_subplot(111)
 
-scatter = plt.scatter(df['input_size'], df['time'], c=df[f'{io}_channels'], cmap='cool', s=100)
+scatter = plt.scatter(fir_df['flops'], fir_df['time'], s=15, label='LEA (FIR)')
+scatter = plt.scatter(cpu_df['flops'], cpu_df['time'], s=15, label='CPU')
 
-if io == 'in':
-    plt.colorbar(scatter, label='Input Channels')
-else:
-    plt.colorbar(scatter, label='Output Channels')
-
-plt.xlabel('Input Size')
+plt.xlabel('MegaFLOPs')
 plt.ylabel('Time (s)')
-plt.title(f'Convolution benchmark (Kernel Size: {kernel_size})')
+plt.title(f'2D Convolution (Kernel Size: 3)')
+
+plt.legend(markerscale=2)
 
 fig.tight_layout(pad=0.1)
-plt.savefig(f'figures/{op}_{io}_channels_{kernel_size}.png', dpi=500)
+plt.savefig(f'flops_time.png', dpi=500)
